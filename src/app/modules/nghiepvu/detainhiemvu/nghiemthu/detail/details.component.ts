2d3e68cc47325790205d66f1117eaf01b9c9f54c
vantuanmigi <tuan.le@migitek.com>
2023-07-30 15:04:40 +0700

    fix

--- src/app/modules/nghiepvu/detainhiemvu/nghiemthu/detail/details.component.ts
+++ src/app/modules/nghiepvu/detainhiemvu/nghiemthu/detail/details.component.ts
@@ -11,6 +11,8 @@ import { Subscription, takeUntil } from 'rxjs';
 import {
     UntypedFormBuilder,
     UntypedFormGroup,
+    FormGroup,
+    AbstractControl,
     Validators,
 } from '@angular/forms';
 import { MessageService } from 'app/shared/message.services';
@@ -39,6 +41,7 @@ export class DetailsComponent implements OnInit {
     public listYears = [];
     public actionType = null;
     public method;
+    public form: FormGroup;
     constructor(
         private _formBuilder: UntypedFormBuilder,
         public _activatedRoute: ActivatedRoute,
@@ -66,6 +69,96 @@ export class DetailsComponent implements OnInit {
         this.geListYears();
     }
 
+    initForm() {
+        this.form = this._formBuilder.group({
+            maDeTai: [null],
+            method: [null],
+            tenDeTai: [null, [Validators.required]],
+            maTrangThai: [null],
+            tenCapQuanLy: [null],
+            isEmail: false,
+            canCuThucHien: [null],
+            keHoach: [null],
+            capQuanLy: [null, [Validators.required]],
+            vanBanChiDaoSo: [null],
+            tenLinhVucNghienCuu: [null],
+            linhVucNghienCuu: [],
+            noiDungGuiMail: [null],
+            noiDung: [null],
+            keHoachTiepTheo: [null],
+            dexuatKienNghi: [null],
+            thang: [null],
+            soLanGiaHan: [null],
+            nam: new Date().getFullYear(),
+            //LINHVUCNGHIENCUU: this._formBuilder.array([]),
+            tenDonViChuTri: [null],
+            donViChuTri: [null, [Validators.required]],
+            thoiGianThucHienTu: [null, [Validators.required]],
+            thoiGianThucHienDen: [null, [Validators.required]],
+
+            chuNhiemDeTaiInfo: '',
+            chuNhiemDeTai: [null, [Validators.required]],
+            gioiTinh: 0,
+            hocHam: [null],
+            hocVi: [null],
+            donViCongTac: [null],
+
+            dongChuNhiemDeTaiInfo: '',
+            dongChuNhiemDeTai: [null, [Validators.required]],
+            gioiTinhDongChuNhiem: 0,
+            hocHamDongChuNhiem: [null],
+            hocViDongChuNhiem: [null],
+            donViCongTacDongChuNhiem: [null],
+
+            thuKyDeTaiInfo: '',
+            thuKyDeTai: [null],
+            gioiTinhThuKy: 0,
+            hocHamThuKy: [null],
+            hocViThuKy: [null],
+            donViCongTacThuKy: [null],
+
+            danhSachThanhVien: this._formBuilder.array([]),
+            danhSachThanhVienHD: this._formBuilder.array([]),
+            danhSachThanhVienHDXT: this._formBuilder.array([]),
+            tenNguonKinhPhi: [null],
+            nguonKinhPhi: [null, [Validators.required]],
+            tongKinhPhi: [null, [Validators.required]],
+            phuongThucKhoanChi: [null],
+            kinhPhiKhoan: [null],
+            kinhPhiKhongKhoan: [null],
+
+            tinhCapThietCuaDeTaiNhiemVu: [null],
+            mucTieu: [null],
+            nhiemVuVaPhamViNghienCuu: [null],
+            ketQuaDuKien: [null],
+            kienNghiDeXuat: [null],
+
+            listFolderFile: this._formBuilder.array([]),
+
+            listFolderFileThucHien: this._formBuilder.array([]),
+            listFolderFileTamUng: this._formBuilder.array([]),
+            listTienDoCongViec: this._formBuilder.array([]),
+            listHDXD: this._formBuilder.array([]),
+            listHDNT: this._formBuilder.array([]),
+            listFolderHSDK: this._formBuilder.array([]),
+            listFolderHSXD: this._formBuilder.array([]),
+            listFolderBanGiao: this._formBuilder.array([]),
+            listFolderQuyetToan: this._formBuilder.array([]),
+            listFolderHSNT: this._formBuilder.array([]),
+
+            // listFile1: this._formBuilder.array([]),
+            // listFile2: this._formBuilder.array([]),
+            // listFile3: this._formBuilder.array([]),
+            // listFile4: this._formBuilder.array([]),
+            // listFile5: this._formBuilder.array([]),
+            // listFile6: this._formBuilder.array([]),
+        });
+    }
+
+    get f(): { [key: string]: AbstractControl } {
+        return this.form.controls;
+    }
+
     geListYears() {
         this.getYearSubscription = this._serviceApi
             .execServiceLogin('E5050E10-799D-4F5F-B4F2-E13AFEA8543B', null)
@@ -90,5 +183,7 @@ export class DetailsComponent implements OnInit {
         });
     }
 
-    onSubmit() {}
+    onSubmit() {
+        console.log(this.form.value);
+    }
 }
