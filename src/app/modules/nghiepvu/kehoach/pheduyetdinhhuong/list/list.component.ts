f0c80c8228d96b485e8a9106914c0068e976e686
vantuanmigi <tuan.le@migitek.com>
2023-08-03 21:21:35 +0700

    ss

--- src/app/modules/nghiepvu/kehoach/pheduyetdinhhuong/list/list.component.ts
+++ src/app/modules/nghiepvu/kehoach/pheduyetdinhhuong/list/list.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+    ViewEncapsulation,
+} from '@angular/core';
 import { Subject, Subscription, takeUntil } from 'rxjs';
 import { MessageService } from 'app/shared/message.services';
 import { UserService } from 'app/core/user/user.service';
@@ -15,10 +22,9 @@ import { ListdinhhuongService } from '../../dinhhuong/listdinhhuong.service';
 @Component({
     selector: 'component-list',
     templateUrl: './list.component.html',
-    styleUrls: ['./list.component.scss']
+    styleUrls: ['./list.component.scss'],
 })
 export class ApiPheduyetdinhhuongListComponent implements OnInit {
-
     public selectedYear: [number];
     public selectedStatus: string;
     public actionClick: string = null;
@@ -31,12 +37,12 @@ export class ApiPheduyetdinhhuongListComponent implements OnInit {
     public checked;
     public getDinhHuongSubcription: Subscription;
     public listDinhHuong = [];
-    public selectedGrid:[{}];
-    public userLogin={EMAIL:'',ORGID:'124'};
+    public selectedGrid: [{}];
+    public userLogin = { EMAIL: '', ORGID: '124' };
     sizes: any[] = [
-        { 'size': '0', 'diameter': '16000 km' },
-        { 'size': '1', 'diameter': '32000 km' }
-      ];
+        { size: '0', diameter: '16000 km' },
+        { size: '1', diameter: '32000 km' },
+    ];
     /**
      * Constructor
      */
@@ -50,88 +56,95 @@ export class ApiPheduyetdinhhuongListComponent implements OnInit {
         private _functionService: FunctionService,
         private _serviceApi: ServiceService,
         private _listdinhhuongService: ListdinhhuongService,
-       
+
         private el: ElementRef
     ) {
-       
-        this._activatedRoute.queryParams
-        .subscribe(params => {
-          
-          if(params?.type){
-            this.actionClick = params?.type
-          }else{
-            this.actionClick = null
-            this.timKiem();
-          }
-        }
-      
-      );
-     
+        this._activatedRoute.queryParams.subscribe((params) => {
+            if (params?.type) {
+                this.actionClick = params?.type;
+            } else {
+                this.actionClick = null;
+                this.timKiem();
+            }
+        });
     }
 
     ngOnInit() {
         this.getUserLogin();
         this.actionClick = null;
         this._listdinhhuongService.getValueYear().subscribe((values: any) => {
-            if (values){
+            if (values) {
                 this.listYears = values;
                 // this.listYears.push({"NAME":2024,"ID":2024});
                 // this.listYears.push({"NAME":2025,"ID":2025})
             }
-               
-        })
+        });
         // this.getStatusSubscription = this._listdinhhuongService.getValueStatus().subscribe((values: any) => {
         //     if (values)
         //         this.listStatus = values;
         // })
-        this.listStatus =[{"MA_TRANG_THAI":"","TEN_TRANG_THAI":"Tất cả"},
-            {"MA_TRANG_THAI":"Y_CAU_HIEU_CHINH","TEN_TRANG_THAI":"Yêu cầu hiệu chỉnh"},
-        {"MA_TRANG_THAI":"CHO_PHE_DUYET","TEN_TRANG_THAI":"Chờ phê duyệt"},
-        {"MA_TRANG_THAI":"DA_PHE_DUYET","TEN_TRANG_THAI":"Đã duyệt"}]
-        this.selectedYear =[((new Date()).getFullYear())];
-        this.selectedStatus='';
+        this.listStatus = [
+            { MA_TRANG_THAI: '', TEN_TRANG_THAI: 'Tất cả' },
+            {
+                MA_TRANG_THAI: 'Y_CAU_HIEU_CHINH',
+                TEN_TRANG_THAI: 'Yêu cầu hiệu chỉnh',
+            },
+            { MA_TRANG_THAI: 'CHO_PHE_DUYET', TEN_TRANG_THAI: 'Chờ phê duyệt' },
+            { MA_TRANG_THAI: 'DA_PHE_DUYET', TEN_TRANG_THAI: 'Đã duyệt' },
+        ];
+        this.selectedYear = [new Date().getFullYear()];
+        this.selectedStatus = '';
         this.timKiem();
     }
 
-
-    onApiSelected(object: any): void {
-
-    }
+    onApiSelected(object: any): void {}
 
     addNew(): void {
         this.actionClick = 'THEMMOI';
     }
 
-    tonghop(status){
+    tonghop(status) {
         this.addNew();
-        let arr = this.listDinhHuong.filter(c => c.state==true);
-        let listKeHoach =[];
-        if(arr !=undefined && arr.length >0){
-          for(let i=0;i< arr.length;i++){
-            if(arr[i] !=undefined && arr[i].listKeHoach !=undefined && arr[i].listKeHoach.length >0){
-              for(let j=0;j<arr[i].listKeHoach.length;j++){
-                let chitiet = arr[i].listKeHoach[j];
-                chitiet.maDonVi = arr[i].maDonVi;
-                listKeHoach.push(arr[i].listKeHoach[j]);
-              }
+        let arr = this.listDinhHuong.filter((c) => c.state == true);
+        let listKeHoach = [];
+        if (arr != undefined && arr.length > 0) {
+            for (let i = 0; i < arr.length; i++) {
+                if (
+                    arr[i] != undefined &&
+                    arr[i].listKeHoach != undefined &&
+                    arr[i].listKeHoach.length > 0
+                ) {
+                    for (let j = 0; j < arr[i].listKeHoach.length; j++) {
+                        let chitiet = arr[i].listKeHoach[j];
+                        chitiet.maDonVi = arr[i].maDonVi;
+                        listKeHoach.push(arr[i].listKeHoach[j]);
+                    }
+                }
             }
-            
-          }
         }
-        let kehoach = {listKeHoach:listKeHoach,capTao:'TCT'}
+
+        let kehoach = { listKeHoach: listKeHoach, capTao: 'TCT' };
         this._serviceApi.dataKeHoach.next(kehoach);
-        this._router.navigateByUrl('nghiepvu/kehoach/pheduyetdinhhuong?type='+status);
+        this._router.navigateByUrl(
+            'nghiepvu/kehoach/pheduyetdinhhuong?type=' + status
+        );
     }
 
+    delays() {}
+
     checkAll(ev) {
-        this.listDinhHuong.filter(c => c.tongHop==false).forEach(x => x.state = ev.target.checked);
-        
+        this.listDinhHuong
+            .filter((c) => c.tongHop == false)
+            .forEach((x) => (x.state = ev.target.checked));
+
         console.log(this.listDinhHuong);
-      }
-      
-      isAllChecked() {
-        return this.listDinhHuong.filter(c => c.tongHop==false).every(_ => _.state);
-      }
+    }
+
+    isAllChecked() {
+        return this.listDinhHuong
+            .filter((c) => c.tongHop == false)
+            .every((_) => _.state);
+    }
 
     // getListDinhHuong() {
     //     this.getDinhHuongSubcription = this._serviceApi.execServiceLogin("F217F0FD-B9AA-4ADC-9EDE-75717D8484FD", [{"name":"MA_TRANG_THAI","value":""},{"name":"NAM","value":(new Date()).getFullYear()},{"name":"ORGID","value":"115"}]).subscribe((data) => {
@@ -140,47 +153,54 @@ export class ApiPheduyetdinhhuongListComponent implements OnInit {
     //     })
     // }
 
-    timKiem(){
-        let nam ="";
-         if(this.selectedYear != null && this.selectedYear.length >0 ){
+    timKiem() {
+        let nam = '';
+        if (this.selectedYear != null && this.selectedYear.length > 0) {
             nam = this.selectedYear.join(',');
-         }
-        
-         this.selectedYear 
-        this.getDinhHuongSubcription = this._serviceApi.execServiceLogin("038D4EB5-55D0-49C4-8FDB-C242E6759955", [{"name":"MA_TRANG_THAI","value":this.selectedStatus},{"name":"NAM_LIST","value":nam},{"name":"PAGE_NUM","value":this.pageIndex},{"name":"PAGE_ROW_NUM","value":this.pageSize}]).subscribe((data) => {
-            this.listDinhHuong = data.data || [];
-             if(data.data != null && data.data.length >0){
-                this.length = data.data[0].TotalPage;
-             }
-             
-         })
+        }
+
+        this.selectedYear;
+        this.getDinhHuongSubcription = this._serviceApi
+            .execServiceLogin('038D4EB5-55D0-49C4-8FDB-C242E6759955', [
+                { name: 'MA_TRANG_THAI', value: this.selectedStatus },
+                { name: 'NAM_LIST', value: nam },
+                { name: 'PAGE_NUM', value: this.pageIndex },
+                { name: 'PAGE_ROW_NUM', value: this.pageSize },
+            ])
+            .subscribe((data) => {
+                this.listDinhHuong = data.data || [];
+                if (data.data != null && data.data.length > 0) {
+                    this.length = data.data[0].TotalPage;
+                }
+            });
     }
     getUserLogin() {
-
-            this._serviceApi.execServiceLogin("EEE8942F-F458-4B58-9B5C-4A0CEE3A75E8", [{"name":"USERID","value":"STR"}]).subscribe((data) => {
+        this._serviceApi
+            .execServiceLogin('EEE8942F-F458-4B58-9B5C-4A0CEE3A75E8', [
+                { name: 'USERID', value: 'STR' },
+            ])
+            .subscribe((data) => {
                 this.userLogin = data.data || {};
-            })
-        
+            });
     }
 
     ngOnDestroy() {
-        this.getDinhHuongSubcription.unsubscribe()
+        this.getDinhHuongSubcription.unsubscribe();
         //this.getYearSubscription.unsubscribe()
         //this.getStatusSubscription.unsubscribe()
     }
 
-     //phân trang
-     length = 0;
-     pageSize = 20;
-     pageIndex = 0;
-     pageSizeOptions = [10, 20, 50,100];
-     showFirstLastButtons = true;
-   
-     handlePageEvent(event: PageEvent) {
-       this.length = event.length;
-       this.pageSize = event.pageSize;
-       this.pageIndex = event.pageIndex;
-       this.timKiem();
-     }
+    //phân trang
+    length = 0;
+    pageSize = 20;
+    pageIndex = 0;
+    pageSizeOptions = [10, 20, 50, 100];
+    showFirstLastButtons = true;
 
+    handlePageEvent(event: PageEvent) {
+        this.length = event.length;
+        this.pageSize = event.pageSize;
+        this.pageIndex = event.pageIndex;
+        this.timKiem();
+    }
 }
